8b807ec4217d0196c7fbd2b5799bd7563271dbbd
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-09-23 02:10:28 +0530

    finish gallary

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id="awards">
+    <div className='app__wrapper_info'>
+      <SubHeading title='Award & recognition' />
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+    </div>
+    
   </div>
 );
 
